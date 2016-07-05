3c7310c3a32b38d392eedfe65ebe4ff5585ef0b2
dayudodo <dayudodo@gmail.com>
2016-07-05 13:44:12 +0800

    complete valid4ABC

--- spec/firstTest.js
+++ spec/firstTest.js
@@ -5,23 +5,36 @@ describe('Base Valid', function() {
 	});
 	it('should validAAA', function() {
 		expect(validAAA('b1 b1 b1')).toBe(true)
+		expect(validAAA_reg('b1 b1 b1')).toBe(true)
 	});
 	it('should validAAA false', function() {
 		expect(validAAA('b1 b1   b2')).toBe(false)
 	});
 
 	describe('valid4A group', function() {
-		it('should valid4A true', function() {
-			expect(valid4A('b1 b1 b1 b1')).toBe(true)
+		it(`0 should valid4A 'b1 b1 b1 b1' true`, function() {
+			let str = 'b1 b1 b1 b1'
+			expect(valid4A(str)).toBe(true)
 		});
-		it('should valid4A false', function() {
-			expect(valid4A('b1 b2 b1 b1')).toBe(false)
+		it("0.0 should valid4A 'b1 b1 b1 b1' true", function() {
+			let str = 'b1 b1 b1 b1'
+			expect(valid4A_reg(str)).toBe(true)
 		});
-			it('should valid4A false', function() {
-			expect(valid4A('b1 b1 b2 b1')).toBe(false)
+		it(`1 should valid4A  false`, function() {
+			let str = 'b1 b2 b1 b1'
+			expect(valid4A(str)).toBe(false)
 		});
-		it('should valid4A false', function() {
-			expect(valid4A('b1 b1 b1 b2')).toBe(false)
+		it(`2 should valid4A  false`, function() {
+			let str = 'b1 b1 b2 b1'
+			expect(valid4A(str)).toBe(false)
+		});
+		it(`3 should valid4A  false`, function() {
+			let str = 'b1 b1 b1 b2'
+			expect(valid4A(str)).toBe(false)
+		});
+		it('4 should vlaid4A throw error', function() {
+			let str='b1'
+			expect(valid4A.bind(null,str)).toThrowError(/must have/)
 		});
 	});
 
@@ -41,10 +54,107 @@ describe('Base Valid', function() {
 		});
 		it('should validABC throw values error', function() {
 			let str='b1 b2 b4 b5'
-			expect(validABC.bind(null,str)).toThrowError('str must have three value')
+			expect(validABC.bind(null,str)).toThrowError(/must have/)
 		});
+	});
 
+	describe('2ABC group', function() {
+		it('1 should valid2ABC true', function() {
+			let str='b1 b2 b3 b4 b5 b6'
+			expect(valid2ABC(str)).toBe(true)
+		});
+		it('1.1 should valid2ABC true', function() {
+			let str='b1 b2 b3 b5 b4 b6'
+			expect(valid2ABC(str)).toBe(true)
+		});
+		it('1.2 should valid2ABC true', function() {
+			let str='b1 b2 b3 t5 t4 t6'
+			expect(valid2ABC(str)).toBe(true)
+		});
+		it('2 should valid2ABC true', function() {
+			let str='b1 b2 b2 b3 b3 b4'
+			expect(valid2ABC(str)).toBe(true)
+		});
+		it('3 should valid2ABC true', function() {
+			let str='b1 b1 b1 t3 t4 t5'
+			expect(valid2ABC(str)).toBe(true)
+		});
+		it('4 should valid2ABC 非框', function() {
+			let str='b1 b2 b3 b3 b3 b3'
+			expect(valid2ABC(str)).toBe(true)
+		});
+		it('5 should valid2ABC true', function() {
+			let str='b1 b2 b3 zh zh zh'
+			expect(valid2ABC(str)).toBe(true)
+		});
+		it('5.1 should valid2ABC true', function() {
+			let str='b1 b1 b2 b2 b3 b3'
+			expect(valid2ABC(str)).toBe(true)
+		});
+		it('6 should valid2ABC false', function() {
+			let str='b1 b2 t2 b3 b3 b5'
+			expect(valid2ABC(str)).toBe(false)
+		});
+		it('7 should valid2ABC true', function() {
+			let str='fa fa fa b1 t1 zh'
+			expect(valid2ABC(str)).toBe(false)
+		});
+		it('7 should valid2ABC true', function() {
+			let str='fa fa fa b1 t1 zh'
+			expect(valid2ABC(str)).toBe(false)
+		});
+	});
 
+	describe('valid3ABC group', function() {
+		it('valid3ABC 正规九张牌', function() {
+			let str='b1 b2 b3 fa fa fa t1 t2 t3'
+			expect(valid3ABC(str)).toBe(true)
+		});
+		it('valid3ABC 九张 122334123', function() {
+			let str='b1 b2 b2 b3 b3 b4 t1 t2 t3'
+			expect(valid3ABC(str)).toBe(true)
+		});
+		it('valid3ABC 九张 112233456', function() {
+			let str='b1 b1 b2 b2 b3 b3 b4 b5 b6'
+			expect(valid3ABC(str)).toBe(true)
+		});	
+		it('valid3ABC 九张 123445566', function() {
+			let str='b1 b2 b3 b4 b4 b5 b5 b6 b6'
+			expect(valid3ABC(str)).toBe(true)
+		});
+	});
+
+	describe('valid4ABC group', function() {
+		it('valid4ABC 12张牌', function() {
+			let str='b1 b2 b3 fa fa fa t1 t2 t3 t4 t5 t6'
+			expect(valid4ABC(str)).toBe(true)
+			let str1= 'b1 b2 b3 fa fa fa t1 t2 t2 t3 t3 t4'
+			expect(valid4ABC(str1)).toBe(true)
+		});
+		it('valid4ABC 12张牌 前6错位', function() {
+			let str='b1 b2 b2 b3 b3 b4 t1 t2 t2 t3 t3 t4'
+			expect(valid4ABC(str)).toBe(true)
+		});
+		it('valid4ABC 12张牌，中间错位', function() {
+			let str='zh zh zh b1 b2 b2 b3 b3 b4 t1 t2 t3'
+			expect(valid4ABC(str)).toBe(true)
+		});
+		it('valid4ABC 12张牌 前9 112233', function() {
+			let str='b1 b1 b2 b2 b3 b3 b4 b5 b6 b7 b8 b9'
+			expect(valid4ABC(str)).toBe(true)
+		});
+		it('valid4ABC 12张牌 后9 112233', function() {
+			let str='b1 b2 b3 b4 b4 b5 b5 b6 b6 b7 b7 b7'
+			expect(valid4ABC(str)).toBe(true)
+		});
+		it('valid4ABC 12 false', function() {
+			let str='b1 b2 b3 fa fa fa t1 t2 t2 t3 t3 zh'
+			expect(valid4ABC(str)).toBe(false)
+		});
+		it('valid4ABC 12张牌', function() {
+			let str='b1 b2 b3 fa fa fa t1 t2 t3 t4 t5 t6'
+			expect(valid4ABC(str)).toBe(true)
+		});
 
 	});
 
--- spec/tt.js
+++ spec/tt.js
@@ -6,9 +6,18 @@ function tt (argument) {
 	// return name.split("").reverse().join("");
 
 }
+function checkNullAndReturnArr(str) {
+	if (!str) {
+		throw new Error('str is null or undefined')
+	}else if(str instanceof Array){
+		return str
+	}else	{
+		return str.split(/\s+/).sort()
+	}
+}
 function validAA (str) {
-	let result=str.split(/\s+/)
-	if (result.length!=2) {throw new Error(`str${str} must have two value`)};
+	var result=checkNullAndReturnArr(str)
+	if (result.length!=2) {throw new Error(`str${str} must have 2 values`)};
 	//不支持Es6语法，奈何？
 	// let s1=result[0]
 	// let s2=result[1]
@@ -17,35 +26,91 @@ function validAA (str) {
 }
 
 function validAAA (str) {
-	var result=str.split(/\s+/)
-	if (result.length!=3) {throw new Error(`str${str} must have three value`)};
-	// var {s1,s2,s3}=result
-	// let s1=result[0]
-	// let s2=result[1]
-	// let s3=result[2]
+	var result=checkNullAndReturnArr(str)
+	if (result.length!=3) {throw new Error(`str${str} must have 3 values`)};
 	let [s1,s2,s3]=result
 	return s1==s2 && s2==s3
 }
 
+
 function valid4A (str) {
-	var result=str.split(/\s+/)
-	if (result.length!=4) {throw new Error(`str${str} must have three value`)};
-	// var {s1,s2,s3}=result
-	// let s1=result[0]
-	// let s2=result[1]
-	// let s3=result[2]
+	var result=checkNullAndReturnArr(str)
+	if (result.length!=4) {throw new Error(`str${str} must have 4 values`)};
 	let [s1,s2,s3,s4]=result
 	return s1==s2 && s2==s3 && s3==s4
 }
 
+
 function validABC(str){
-	if (!str) {
-		throw new Error('str is null or undefined')
-	}
-	var result=str.split(/\s+/)
-	if (result.length!=3) {throw new Error('str must have three value')};
+	var result=checkNullAndReturnArr(str)
+	if (result.length!=3) {throw new Error(`str${str} must have 3 values`)};
 	let [s1,s2,s3] = result
-	//判断首字母是否相同以及 是否是1，2，3这样的顺序
+	//判断首字母是否相同(判断相同花色)以及 是否是1，2，3这样的顺序
 	let isABC = (s2[1]-1==s1[1] && s3[1]-1 == s2[1]) && (s1[0]==s2[0] && s2[0]==s3[0])
 	return isABC
+}
+
+function isABCorAAA (str) {
+	return validAAA(str) || validABC(str)
+}
+function valid2ABC (str) { //like 123456 or 122334,233445这样的牌型
+	var result = checkNullAndReturnArr(str)
+	if (result.length!=6) {throw new Error(`str${str} must have 6 values`)};
+	let [s1,s2,s3,s4,s5,s6] = result
+	let frontThree = [s1,s2,s3]
+	let lastThree  = [s4,s5,s6]
+	//特殊情况，比如112233的情况？
+	if (s1==s2 && s3==s4 && s5==s6) {
+		return validABC([s1,s3,s5])
+	};
+	if (isABCorAAA(frontThree) && isABCorAAA(lastThree)) {
+		return true
+	}
+	else{ //交换2,3 比如将122334中间的两个交换过来，再检查
+		frontThree = [s1,s2,s4]
+		lastThree  = [s3,s5,s6]
+		// console.log(frontThree,lastThree)
+		if (isABCorAAA(frontThree) && isABCorAAA(lastThree)) {
+			return true
+		}else{
+			return false
+		}
+	}
+}
+
+function valid3ABC (str) {
+	var result = checkNullAndReturnArr(str)
+	if (result.length!=9) {throw new Error(`str${str} must have 9 values`)};
+	let frontThree = result.slice(0,3)
+	let lastSix = result.slice(3,9)
+	let frontSix = result.slice(0,6)
+	let lastThree = result.slice(6,9)
+	// console.log(frontThree,lastSix)
+	if (isABCorAAA(frontThree) && valid2ABC(lastSix)) {
+		return true
+	}else if (valid2ABC(frontSix) && isABCorAAA(lastThree)) {
+		return true
+	}else{
+		return false
+	}
+}
+function valid4ABC (str) {
+	var result = checkNullAndReturnArr(str)
+	if (result.length!=12) {throw new Error(`str${str} must have 9 values`)};
+	let frontThree = result.slice(0,3)
+	let lastNine = result.slice(3,12)
+	let frontNine = result.slice(0,9)
+	let lastThree = result.slice(9,12)
+	let frontSix = result.slice(0,6)
+	let lastSix = result.slice(6,12)
+	if (valid2ABC(frontSix) && valid2ABC(lastSix)) {
+		return true
+	}
+	if (isABCorAAA(frontThree) && valid3ABC(lastNine)) {
+		return true
+	}else if (valid3ABC(frontNine) && isABCorAAA(lastThree)) {
+		return true
+	}else{
+		return false
+	}
 }
