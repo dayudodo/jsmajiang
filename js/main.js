2f18d8c7792f3930055e4787fa4c7d065a0f455b
dayudodo <dayudodo@gmail.com>
2016-07-06 23:31:02 +0800

    show images under input tag

--- js/main.js
+++ js/main.js
@@ -1,39 +1,35 @@
-		var MJInput=React.createClass({
-		 getInitialState:function(){
-		 	return {
-		 		text: ''
-		 	  , single: ''
-		 	  , results: []
-		 	}
-		 },
-		 onChange:function(e){
-		 	// var imgSrc= "image/"+ e.target.value + ".png"
+var MJInput=React.createClass({
+ getInitialState:function(){
+ 	return {
+ 		text: ''
+ 	  , single: ''
+ 	  , results: []
+ 	}
+ },
+ onChange:function(e){
+ 	this.setState({text: e.target.value})
+ },
+ handleSubmit: function(e){
+ 	e.preventDefault()
+ 	// this.setState({single: imgSrc})
+ 	if (this.state.text || this.state.text=='') {
+		this.setState( {results:  checkValidAndReturnArr(this.state.text) } )
+	}
+ },
+ render: function(){
+  	const allImages = this.state.results.map(function(item,index){
+  		let imgSrc = `image/${item}.png` 
+  		return <img key={index} src={imgSrc} />
+  	})
+    return(
+      <div>
+	      <form onSubmit={this.handleSubmit}>
+	        <input onChange={this.onChange} value={this.state.text} />
+	      </form>
+          { allImages }
+      </div>
+    );
+ }
+});
 
-		 	this.setState({text: e.target.value})
-		 	
-		 },
-		 handleSubmit: function(e){
-		 	e.preventDefault()
-		 	// this.setState({single: imgSrc})
-		 	if (this.state.text || this.state.text=='') {
-				this.setState( {results:  checkValidAndReturnArr(this.state.text) } )
-			}
-		 },
-
-		  render: function(){
-		  	const allImages = this.state.results.map(function(item,index){
-		  		let imgSrc = `image/${item}.png` 
-		  		return( <img key={index} src={imgSrc} />)
-		  	})
-		    return(
-		    	<div>
-			      <form onSubmit={this.handleSubmit}>
-			        <input onChange={this.onChange} value={this.state.text} />
-			        { allImages }
-			      </form>
-		      </div>
-		      );
-		  }
-		});
-
-		ReactDOM.render( <MJInput />,  document.getElementById('app') );
+ReactDOM.render( <MJInput />,  document.getElementById('app') );
