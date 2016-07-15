fec044e86657e91ca7aba582ee0c1237f8563456
dayudodo <dayudodo@gmail.com>
2016-07-15 14:56:03 +0800

    three players can start game,all have 13 zhang pai

--- app.js
+++ app.js
@@ -7,12 +7,29 @@ var express = require('express')
   , io = require('socket.io').listen(server);
 var _ = require('lodash');
 
+Array.prototype.repeat= function(times){
+  var result = []
+  for (var i = 0; i < times; i++) {
+    this.map(item=>{
+      result.push(item)
+    })
+  }
+  return result
+}
+
 var connections = []
 var ArrayPlayer = []
-var current_player 
-// ArrayPlayer.prototype.toString = function(){
 
-// }
+var BING = ['b1','b2','b3','b4','b5','b6','b7','b8','b9']
+var TIAO   = ['t1','t2','t3','t4','t5','t6','t7','t8','t9']
+// 中风、发财、白板(电视)，为避免首字母重复，白板用电视拼音，字牌
+var ZHIPAI    = ['zh','fa','di']
+
+var all_single_pai=BING.concat(TIAO).concat(ZHIPAI)
+var all_pai = BING.repeat(4).concat(TIAO.repeat(4)).concat(ZHIPAI.repeat(4))
+// console.log(_.shuffle(all_pai), all_pai.length)
+var RoomName = 'room'
+
 server.listen(3000, function(){
   console.log("Express server listening on port " + 3000);
 });
@@ -23,13 +40,12 @@ app.use(express.static('./'))
 
 io.sockets.on('connection', function (socket) {
 
-  // 打印握手信息
-  // console.log(socket.handshake);
-
     // 玩家对象
     var player = {
-      id:socket.id,
-      username:undefined
+        id:socket.id
+      , username:undefined
+      , ready: false
+      , first: false
     }
 
     socket.on('disconnect', function () {  
@@ -50,7 +66,7 @@ io.sockets.on('connection', function (socket) {
     })
 
     connections.push(socket);
-    console.log("Connected: %s connectionss", connections.length);
+    console.log("Connected: %s connections", connections.length);
 
     function joined(socket, obj){
       socket.emit('joined', obj)
@@ -72,25 +88,53 @@ io.sockets.on('connection', function (socket) {
           socket.emit('room_full')
           console.log(nameStr)
         }else {
+          if ( ArrayPlayer.length ==0 ) {
+            player.first = true;
+          }
           player.username = new_player.username
+          socket.join(RoomName,function(){
+            socket.to(RoomName).emit('room_enter', player.username)
+            console.log("%s join room ", player.username)
+          })
           ArrayPlayer.push(player)
-          socket.emit('login')
-          socket.broadcast.emit('joined', ArrayPlayer)
+          socket.emit('login', ArrayPlayer) //给自己发个login, 表明服务器已经允许你登录
+          socket.broadcast.emit('joined', ArrayPlayer) // 给其它人发joined, 表明自己已经加入游戏中
           nameStr = ArrayPlayer.map(item=>item.username)
           console.log(player.username + ":joined! Broadcast to %s player", nameStr)
         }
       }
     })
 
-    socket.on('start game', function(){
-      let index = _.findIndex(ArrayPlayer, { id: socket.id })
-      let current_player = ArrayPlayer[index]
-      console.log("current_player ready to start:", current_player.username)
+    socket.on('ready_server', function(){
+      let current_player = _.find(ArrayPlayer, { id: socket.id })
+      current_player.ready = true
+      socket.emit('ready_client', current_player.username)
+      socket.to('room').emit('ready_client', current_player.username)
+      console.log("%s is ready to start:", current_player.username)
+      // 如果所有的人都准备好了，就开始游戏！
+      let player_ready_count = ArrayPlayer.filter( item=>item.ready ).length
+      console.log('ready count:',player_ready_count)
+      let allReady = ( player_ready_count == 3 ) 
+      if ( allReady ) {
+        let yiShou = all_pai.splice(0,13)
+        socket.emit('game_start', yiShou)
+        console.log('all players ready, start game')
+        let others = ArrayPlayer.map(item=>{
+          return _.find(connections, { id: item.id})
+        })
+        // console.log(others.length)
+        others.forEach(otherSocket=>{
+          otherSocket.emit('game_start', all_pai.splice(0,13))
+        })
+        
+      }
+    })
+
+    socket.on('chat_cast',function(info){
+      socket.to('room').emit('chat_cast', info)
     })
 
 
-      // socket.emit('room_full')
-      // console.log("Already have 3 connectionss in here!")
 
 });
 
--- components/play.jsx
+++ components/play.jsx
@@ -5,19 +5,24 @@ import io from 'socket.io-client'
 import PlayerImages from './PlayerImages';
 
 
-
 var Play=React.createClass({
   getInitialState() {
       return {
             status: 'disconnect'
           , text: ''
+          , chatText: ''
           , username: ''
+          , ready: false
           , ArrayPlayer: []
           , info_room: ''
+          , results: []
       };
   },
   onChange:function(e){
       this.setState({ text: e.target.value })
+  },
+  chatChange:function(e){
+      this.setState({ chatText: e.target.value })
   },
   handleSubmit: function(e){
     e.preventDefault()
@@ -30,46 +35,95 @@ var Play=React.createClass({
       })
     }
   },
-  startGame(){
-    console.log(this.state.username + "start game")
-    this.socket.emit('start game')
+  handleChatSubmit:function(e){
+    e.preventDefault()
+    let isEmpty = this.state.chatText.replace(/\s+/g).length == 0
+    if (isEmpty) { 
+      alert("用户名不能为空") 
+    }else {
+      this.socket.emit('chat_cast',{
+         username: this.state.username
+       , chatText: this.state.chatText
+      })
+    }
+  },
+  startGame: function(){
+    // console.log(this.state.username + "ready_server")
+    this.socket.emit('ready_server')
+  },
+  show_info_room:function(ArrayPlayer){
+    let filtered = ArrayPlayer.filter(item=>item.username!=this.state.username)
+    let allNames= filtered.map(item=>item.username)
+    this.setState({ info_room: allNames.join(',') + '进入房间' })
   },
   componentWillMount() {
       this.socket = io('http://localhost:3000');
-
       this.socket.on('connect',()=>{ this.setState({ status: '服务器连接成功'})})
 
       this.socket.on('disconnect',()=>{ 
-        this.setState({ status: '服务器已经断开'}) 
-        this.setState({ username: '', info_room: ''})
+        this.setState({ 
+            status: '服务器已经断开'
+          , username: '', info_room: ''
+          , ready: false
+        }) 
       })
 
       this.socket.on('room_full',()=>{ this.setState({ info_room: '房间已满'})})
 
       this.socket.on('joined', (ArrayPlayer)=>{ 
-        let filtered = ArrayPlayer.filter(item=>item.username!=this.state.username)
-        let allNames= filtered.map(item=>item.username)
-        this.setState({ info_room: allNames.join(',') + '进入房间' })
+        this.show_info_room(ArrayPlayer)
       })
 
-      this.socket.on('login',()=>{ this.setState({username: this.state.text }) })
+      this.socket.on('login',(ArrayPlayer)=>{ 
+        this.setState({username: this.state.text }) 
+        this.show_info_room(ArrayPlayer)
+      })
+
+      this.socket.on('room_enter',(info)=>{
+        console.log("new user entered", info)
+      })
+      this.socket.on('chat_cast',(info)=>{
+        console.log("%s : %s", info.username, info.chatText)
+      })
+
+      this.socket.on('ready_client', (info)=>{
+        this.setState({ 
+            ready: true
+          , status: this.state.username +'已准备，等待其它玩家加入'
+        })
+        console.log('%s is ready to start', info)
+      })
+
+      this.socket.on('game_start', (serverData)=>{
+        this.setState({ 
+            status: '游戏开始'
+          , results: serverData
+        })
+      })
+
+
   },
+
   render: function(){
     
      return(
        <div>
         <h1>{ this.state.status }</h1>
         <h1>{ this.state.info_room }</h1>
+       <button onClick={ this.send_test }>test</button> 
         { (this.state.username.length == 0) ? 
            <form onSubmit={this.handleSubmit}  >
              用户名：<input onChange={this.onChange} value={this.state.text} /> 
            </form> : 
            <div>{ this.state.username } 登入
-             <button onClick={ this.startGame }>开始</button> 
+             { this.state.ready? null : <button onClick={ this.startGame }>开始</button> }
+             <form onSubmit={this.handleChatSubmit}  >
+             发信息：<input onChange={this.chatChange} value={this.state.chatText} /> 
+             </form>
            </div>
         }
 
-        <PlayerImages results={ ['b1','b2','b3','b4','zh','zh','fa','fa'] } />
+        <PlayerImages results={ this.state.results.sort() } />
        </div>
      );
   }
