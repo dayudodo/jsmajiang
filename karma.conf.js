712ea50f947bf7c278b02b1872dc84c1eef9c62d
Adam Lu <ange@AdamdeMacBook-Pro.local>
2017-01-04 13:45:31 +0800

    regenerate karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -1,72 +1,47 @@
 // Karma configuration
-// Generated on Mon Jul 04 2016 12:21:46 GMT+0800 (中国标准时间)
-
+// Generated on Sun Jan 01 2017 13:51:18 GMT+0800 (CST)
 module.exports = function(config) {
   config.set({
-
     // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: './spec',
-
-
+    basePath: '',
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jasmine'],
-
-
     // list of files / patterns to load in the browser
     files: [
-        "*.js"
+      // 'tt.js',
+      // {pattern: 'spec/firstTest.js', included: false}
+      // 'spec/*.js'
+      'spec/tt.js',
+      'spec/reg_version.js',
+      'spec/firstTest.js'
     ],
-
-
     // list of files to exclude
     exclude: [
     ],
-
-
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-        'tt.js':'coverage'
     },
-
-
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress','coverage'],
-    coverageReporter: {
-        type : 'html',
-        dir : 'coverage/'
-    },
-
-
+    reporters: ['progress'],
     // web server port
     port: 9876,
-
-
     // enable / disable colors in the output (reporters and logs)
     colors: true,
-
-
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
-
-
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
-
-
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['Chrome'],
-
-
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
     singleRun: false,
-
     // Concurrency level
     // how many browser should be started simultaneous
     concurrency: Infinity
