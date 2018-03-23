0da250520801f4c6f44a6f2b659e178992ad4253
dayudodo <dayudodo@gmail.com>
2018-03-23 12:26:32 +0800

    typescript directTest.ts

--- spec/directTest.ts
+++ spec/directTest.ts
@@ -1,10 +1,9 @@
 //专门测试一些单独的方法，直接放在其它测试框架中貌似也并不太好用，关键是我想跑一跑调试！
 import { Majiang } from "../server/Majiang";
 
-let str = "b1 b2 b3 b4 b5 b6 t4 t5 t6 fa fa fa zh ";
-let na_pai = 'zh'
-console.log(Majiang.HuPaiNames(str, na_pai))
-
+let str: string = "b1 b2 b3 b4 b5 b6 t4 t5 t6 fa fa fa zh ";
+let na_pai: string = "zh";
+console.log(Majiang.HuPaiNames(str, na_pai));
 
 str = "b1 b2 b3 b4 b5 b6 b7 b8 b9 b1 b2 b3 b4 ";
 na_pai = "b4";
