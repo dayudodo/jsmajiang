0615a7605a2428fe5f8627047f4610561982bd0b
dayudodo <dayudodo@gmail.com>
2018-04-26 18:20:31 +0800

    create room success

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ const app = express();
 const server = http.createServer(app);
 const wsserver = new WebSocket.Server({ server });
 const g_lobby = new Connector();
-var test_names = ["jack", "rose", "tom","jerry","michael","adam","bruce"];
+var test_names = ["jack", "rose", "tom", "jerry", "michael", "adam", "bruce"];
 
 wsserver.on("connection", socket => {
   socket.id = g_lobby.generate_socket_id();
@@ -48,24 +48,64 @@ wsserver.on("connection", socket => {
   socket.on("message", message => {
     //log the received message and send it back to the client
     let client_message = JSON.parse(message);
+
+    //测试登录
     if (client_message.testlogin) {
       let shift_name = test_names.shift();
       if (!shift_name) {
         throw new Error("没有空余的测试用户名了！");
       }
+      let conn = g_lobby.find_conn_by(socket);
       let s_player = new Player({
         socket: socket,
         username: shift_name
       });
       console.log(`${s_player.username}登录成功，socket_id: ${socket.id}`);
-
+      conn.player = s_player;
       socket.send(
         JSON.stringify({
-          server_login: 'success',
+          server_login: "success",
           userId: socket.id,
           username: s_player.username
         })
       );
+    //创建房间
+    } else if (client_message.create_room) {
+      let conn = g_lobby.find_conn_by(socket);
+      if (!conn.player) {
+        socket = null;
+        console.log(
+          `用户${conn.username}未登录执行了创建房间：${conn.socket.id}`
+        );
+        return;
+      } else {
+        let owner_room = new Room();
+        let room_name = Room.make();
+        if (room_name) {
+          owner_room.id = room_name;
+        } else {
+          console.log("服务器无可用房间了");
+          socket.send(
+            JSON.stringify({
+              server_no_room: "server_no_room"
+            })
+          );
+          return;
+        }
+        owner_room.dong_jia = conn.player; //创建房间者即为东家，初始化时会多一张牉！
+        conn.player.seat_index = 0; //玩家座位号从0开始
+        owner_room.join_player(conn.player); //新建的房间要加入本玩家
+        conn.room = owner_room; //创建房间后，应该把房间保存到此socket的连接信息中
+        console.log(`${conn.player.username}创建了房间${owner_room.id}`);
+        // console.dir(conn)
+        //成功创建房间后要给前端发送成功的消息
+        socket.send(
+          JSON.stringify({
+            server_join_room_ok: "server_join_room_ok",
+            room_name: owner_room.id
+          })
+        );
+      }
     } else {
       socket.send(JSON.stringify({ info: `未知消息${message}` }));
     }
