9123281883c214ace9a52a12f029ed4b4d231f40
dayudodo <dayudodo@gmail.com>
2019-03-29 01:11:20 +0800

    continue test isJiJuhua

---
+++ spec/newhu_test/newPihu.js
@@ -0,0 +1,29 @@
+import test from "ava";
+import { NMajiangAlgo as NMajiangAlgo } from "../../server_build/server/NMajiangAlgo";
+import {PaiConvertor} from "../../server_build/server/PaiConverter"
+// var Majiang = require("../server/Majiang");
+// var mj = new Majiang()
+/**f直接将字符串转换成数类麻将数组 */
+function pais(strs) {
+  return PaiConvertor.pais(strs)
+}
+
+
+test(`is ABC AAA`, function(t) {
+  let str = "b2 b3 b4 b5 b5 b5";
+  let test_arr = pais(str)
+  let result = NMajiangAlgo.isJiJuhua(test_arr)
+  t.is(result, true);
+});
+test(`is abbccd`, function(t) {
+  let str = "b1 b2 b2 b3 b3 b4";
+  let test_arr = pais(str)
+  let result = NMajiangAlgo.isJiJuhua(test_arr)
+  t.is(result, true);
+});
+test(`is abbccd`, function(t) {
+  let str = "b1 b2 b3 b3 b3 b3";
+  let test_arr = pais(str)
+  let result = NMajiangAlgo.isJiJuhua(test_arr)
+  t.is(result, true);
+});
